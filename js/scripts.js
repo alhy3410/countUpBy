f61b97028cc6939fbe92599f6f8ba94296183bf5
ian bevel & andrew lee <student@epicodus.com>
2015-05-28 09:36:32 -0700

    add html file to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 var countUpBy = function(total,increment){
   var numbers = [];
   for(var index = increment; index <= total; index = increment + index){
-
     numbers.push(index);
   }
     return numbers;
 };
+
+$(document).ready(function() {
+  $("form#number").submit(function(event) {
+    var total = parseInt($("input#total").val());
+    var increment = parseInt($("input#increment").val());
+    var result = countUpBy(total, increment);
+
+    $(".allNumbers").text(result);
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('countUpBy', function() {
   it('will count to 20 by 2', function() {
     expect(countUpBy(20,2)).to.eql([2,4,6,8,10,12,14,16,18,20])
   });
+
+  it('will count to 50 by 7', function() {
+    expect(countUpBy(50,7)).to.eql([7,14,21,28,35,42,49])
+  });
 });
